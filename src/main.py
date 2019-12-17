694828e6c131f550965413750819e461dc4eecce
Milykh Svetozar <smilykh@al-enterprise.com>
2019-12-17 09:32:41 +0300

    First lesson

---
+++ src/main.py
@@ -0,0 +1,29 @@
+print('hello, world!')
+print('some text')
+print('hello')
+print(123)
+
+a = 123
+b = 2323
+c = a + b
+b = b - 10
+b = 16
+c = 25
+a = (b - c) * (b + c)
+print(a)
+print(c)
+
+a = 11.23
+a = int(a)
+print(a)
+
+year = 1995
+value = 'I was born in ' + str(year)
+print(value)
+
+print('I was born in', year)
+
+number_int = 123
+number_float = 123.23
+number_result = number_float + number_int
+print(number_float)
