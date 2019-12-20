ee5687fb586a25df137147713600177534e0ce5c
Milykh Svetozar <svt2404@gmail.com>
2019-12-20 22:16:11 +0300

    List example extended

--- src/lists_example.py
+++ src/lists_example.py
@@ -21,11 +21,43 @@ for number in data:
 print(count)
 # напечатать все четные элементы из списка
 print("--")
+data = [1, 2, 3, -3, 2, -3]
+for number in data:
+    if number % 2 == 0:
+        print(number)
+
 # напечатать элементы на четных позициях в списке
 for i in range(len(data)):
     if i % 2 == 0:
         print(data[i])
 print(data[::2])
 # среднее арифметичекое элементов массива
+data = [1, 2, 3, -3, 2, -3]
+print(sum(data) / len(data))
 
 # напечатать все строки, длина которых больше 5
+data = ['hello', 'very long string', 'very very long string']
+for value in data:
+    if len(value) > 5:
+        print(value)
+
+print(ord('a'))
+print(chr(97))
+symbol = 'b'
+upper_symbol = chr(ord(symbol) - 32)
+print(upper_symbol)
+lower_symbol = chr(ord(upper_symbol) + 32)
+print(lower_symbol)
+
+value = 'Hello, world'
+result = ''
+for symbol in value:
+    if 'A' <= symbol <= 'Z':
+        result += chr(ord(symbol) + 32)
+    elif 'a' <= symbol <= 'z':
+        result += chr(ord(symbol) - 32)
+    else:
+        result += symbol
+print(result)
+
+
