4b2584667bc7a0b4c4c5f1c17f4cf387675a7c48
svetomsk <svt2404@gmail.com>
2020-01-14 11:23:25 +0300

    Miner first implementation

---
+++ opp/classes_example.py
@@ -0,0 +1,33 @@
+class Car:
+    def __init__(self, model, color="", horses=0):
+        self.velocity = 0
+        self.model = model
+        self.color = color
+        self.horses = horses
+        # self.unknown_value = None
+
+    def speedUp(self, vel):
+        self.velocity += vel
+
+    def __str__(self):
+        return self.model + " " + self.color
+
+
+bmw = Car("BMW", "Red", 300)
+# bmw.model = "BMW"
+# bmw.color = "Red"
+# bmw.horses = 300
+bmw.speedUp(10)
+print(bmw.velocity)
+
+audi = Car("Audi", "Blue", 315)
+# audi.model = "Audi"
+# bmw.color = "Blue"
+# bmw.horses = 315
+
+print(bmw.model, audi.model)
+print(bmw.color, audi.color)
+
+str_bmw = str(bmw)
+print(bmw)
+print(bmw.__dict__)
---
+++ opp/miner/cell.py
@@ -0,0 +1,7 @@
+class Cell:
+    MINE = -1
+
+    def __init__(self):
+        self.is_open = False
+        self.mines_count = 0
+
