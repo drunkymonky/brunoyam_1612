d8d8dc1e9842995907490a4cb272cb0b33968016
svetomsk <svt2404@gmail.com>
2020-01-27 22:10:14 +0300

    database helper

---
+++ rss_reader/database_helper.py
@@ -0,0 +1,19 @@
+from rss_reader.xml_parser import NewsItem
+from typing import List
+
+
+class DBHelper:
+    def add_news(self, values):
+        """
+        :type values: List[NewsItem]
+        :return:
+        """
+        pass
+
+    def get_news(self, count=100):
+        """
+        Возвращает список всех новостей, отсортированные по дате
+        :rtype: List[NewsItem]
+        """
+        pass
+
